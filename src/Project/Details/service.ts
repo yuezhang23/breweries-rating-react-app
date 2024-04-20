6f345038b0f9b3644eb79d75947d0e27181f509c
yuezhang23 <jackiezy1002@gmail.com>
2024-04-19 22:31:31 -0400

    add admin.ts

--- src/Project/Details/service.ts
+++ src/Project/Details/service.ts
@@ -1,12 +1,33 @@
 import axios from 'axios';
 
+
 export const BREW_API = process.env.BREWERY_API_BASE;
 //set as https://api.openbrewerydb.org/v1/breweries on netlify and in .env 
-
+export const THREE_API = process.env.THREE_API_BASE;
+// set as https://cafe-node-server-06d2fe9af4a2.herokuapp.com/ || http://localhost:4000
 
 export const randomBreweryFromAPI = async () => {
   console.log('API Base URL:', BREW_API);
   const response = await axios.get(`${BREW_API}/random`)
-  return response.data[0];
+  const local = await axios.get(`${THREE_API}/api/admin/breweries/random`)
+  if (response) {
+    return response.data[0];
+  } else {
+    return local.data;
+  }
 }
+
+
+export const getAllBreweries = async () => {
+  const response = await axios.get(`${BREW_API}`);
+  const local = await axios.get(`${THREE_API}/api/admin/breweries`);
+  if (response) {
+    return response.data;
+  } else {
+    return local.data;
+  }
+}
+
+
+
 
---
+++ src/Project/Home/admin.ts
@@ -0,0 +1,47 @@
+import axios from "axios";
+axios.defaults.withCredentials = true;
+
+export const BASE_API = process.env.REACT_APP_API_BASE;
+export const ADMIN_API = `${BASE_API}/api/admin`;
+
+
+export const createBrew = async (brew: any) => {
+    const response = await axios.post(`${ADMIN_API}/breweries`, brew);
+    return response.data;
+};
+
+export const updateBrew = async (brewId: any, brew: any) => {
+    const response = await axios.put(`${ADMIN_API}/breweries/${brewId}`, brew);
+    return response.data;
+};
+
+export const deleteBrew = async (brew: any) => {
+    const response = await axios.delete(
+        `${ADMIN_API}/breweries/${brew._id}`);
+    return response.data;
+};
+
+export const findAllBrews = async () => {
+    const response = await axios.get(`${ADMIN_API}/breweries/`);
+    return response.data;
+};
+
+export const findBrewById = async (id: string) => {
+    const response = await axios.get(`${ADMIN_API}/breweries/${id}`);
+    return response.data;
+};
+
+export const sortBrewByLikes = async (count: number) => {
+    const response = await axios.get(`${ADMIN_API}/breweries/likes/${count}`);
+    return response.data;
+};
+
+export const sortBrewByFollowers = async (count: number) => {
+    const response = await axios.get(`${ADMIN_API}/breweries/followers/${count}`);
+    return response.data;
+};
+
+export const findReviewsByUserId = async (userId: string) => {
+    const response = await axios.get(`${ADMIN_API}/breweries/reviews/${userId}`);
+    return response.data;
+};
