6998bcc463e7be1e6227f9c1baf258df6f45568a
Yanrun <zhuyanrun@Yanruns-MacBook-Pro.local>
2024-04-20 16:32:47 -0400

    brewery detail

--- src/Project/Details/DetailBrewery/index.tsx
+++ src/Project/Details/DetailBrewery/index.tsx
@@ -1,8 +1,66 @@
+import { useLocation, useParams } from "react-router";
+import GoogleComponent from "../GoogleComponent";
+import { useEffect, useState } from "react";
+
+interface Brewery {
+  id: string;
+  name: string;
+  brewery_type: string;
+  phone: string;
+  website_url: string;
+  city: string;
+  state: string;
+  country: string;
+  latitude: string;
+  longitude: string;
+}
+
 function DetailBrewery() {
+  const { detailId } = useParams<{ detailId: string }>();
+  const [brew, setBrew] = useState<Brewery | null>(null);
+
+
+  useEffect(() => {
+    const fetchBrewery = async () => {
+      const url = `https://api.openbrewerydb.org/v1/breweries/${detailId}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setBrew(data);
+    };
+
+    fetchBrewery();
+  }, [detailId]);
+  
+  if (!brew) {
+    return <div>Loading brewery details...</div>; 
+  }
+
+  const renderMap = () => {
+    if ((brew.latitude) && (brew.latitude)) {
+      return <GoogleComponent center={{ lat: parseFloat(brew.latitude), lng: parseFloat(brew.longitude) }}/>
+    }
+  }
   
   return (
     <div className="container-fluid">
-      Temp Details for a Brewery
+      <div className="row">
+        <div className="col-md-6">
+          <div className="card bg-secondary text-white" style={{ height: '400px' }}>
+            <div className="card-body">
+              <h3 className="card-title">{brew.name}</h3>
+              <p className="card-text"><strong>Type:</strong> {brew.brewery_type}</p>
+              <p className="card-text"><strong>Phone:</strong> {brew.phone}</p>
+              <p className="card-text"><strong>Website:</strong>
+                <a href={brew.website_url} className="ms-1 text-decoration-none text-white" target="_blank" rel="noopener noreferrer">{brew.website_url}</a>
+              </p>
+              <p className="card-text"><strong>Address:</strong> {`${brew.city}, ${brew.state}, ${brew.country}`}</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          {renderMap()}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Project/Home/user.tsx
@@ -0,0 +1,14 @@
+import { useSelector } from "react-redux";
+import { ProjectState } from "../store";
+
+function Home() {
+  
+  return (
+    <div className="container-fluid">
+      User Home
+    </div>
+  );
+}
+
+export default Home;
+  
